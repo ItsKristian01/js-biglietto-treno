071008e9543d1b11d5da21ff23e32728f7456ffb
ItsKristian01 <kristianhana11@gmail.com>
2023-10-18 15:52:00 +0200

    add: data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert ("ciao");
+// alert ("ciao");  TEST
+
+// Data
+const userKm = parseInt(prompt("Quanti KM vuoi percorrere?"));
+console.log(userKm, typeof userKm);
+
+const userAge = parseInt (prompt("Quanti anni hai?"));
+console.log(userAge, typeof userAge);
+
+const travelTicket = 0.21 * userKm;
+console.log(travelTicket, typeof travelTicket);
+
+// Program Logic
+
+
+
+
+
+// Output
