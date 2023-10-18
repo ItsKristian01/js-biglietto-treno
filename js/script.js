778df21275b0c959cf3ee474b9e0a2f78bf2c6cb
ItsKristian01 <kristianhana11@gmail.com>
2023-10-18 16:31:02 +0200

    add: program logic

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@
 const userKm = parseInt(prompt("Quanti KM vuoi percorrere?"));
 console.log(userKm, typeof userKm);
 
-const userAge = parseInt (prompt("Quanti anni hai?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
 console.log(userAge, typeof userAge);
 
-const travelTicket = 0.21 * userKm;
+let travelTicket = 0.21 * userKm;
 console.log(travelTicket, typeof travelTicket);
+travelTicket = travelTicket.toFixed(2);
+
 
 // Program Logic
+if (userAge < 18) {
+    console.log("E' Minorenne ha diritto ad uno sconto del 20%");
+} else if (userAge > 65) {
+    console.log("Ha un eta' over 65 ha diritto ad uno sconto del 40%");
+}
 
-
-
-
+if (userAge < 18) {
+    let minorDisc = travelTicket * 0.20;
+    let minorTicket = travelTicket - minorDisc;
+    console.log(minorTicket, typeof minorTicket);
+} else if (userAge > 65) {
+    let overDisc = travelTicket * 0.40;
+    let overTicket = travelTicket - overDisc;
+    console.log(overTicket, typeof overTicket);
+}
 
 // Output
